f399c2dbd0802ab2acd6360a2ee1b5b502b3a36a
Athos <clsaint77@gmail.com>
2025-11-17 16:12:32 -0300

    script.js HomePage + Address

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function mostrarMensagem() {
-  alert("Você clicou no botão! 🎉");
+const modal = document.querySelector(".modal");
+const mascara = document.querySelector(".mascara-modal");
+
+
+function mostrarModal() {
+  modal.style.left = "50%";
+  mascara.style.visibility = "visible";
+}
+
+function fecharModal() {
+  modal.style.left = "-50%";
+  mascara.style.visibility = "hidden";
 }
